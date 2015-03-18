1342ec8f597bec6e5846b80b06730e86d168558a
unknown <chmywoo@gmail.com>
2015-03-18 19:58:33 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
